0e21613b543b08d8e32c94f4a13247f1913727d4
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2020-04-05 17:48:19 -0300

    updating REPL functions

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 const repl = require('repl');
 const fs = require('fs');
 const path = require('path');
+var string_similarity = require('string-similarity');
 
 /* constants */
-const version = "1.0.0"
-const snippets_dir = "./snippets"
+const version = "1.0.0";
+const snippets_dir = "./snippets";
+const threshold_sim = 0.25;
 
 /* library description */
-const library_desc = {}
+const library_desc = {};
+
+/* snippet description */
+const snippets = {};
 
 /* read description of snippets from snippets dir */
 fs.readdir(snippets_dir, (err, files) => {
     files.forEach(file => {
-        // update dictionary with library descriptions
+        var filepath = path.join(snippets_dir, file);
+        var text = fs.readFileSync(filepath, 'utf8');
+        // update dictionaries with library and snippet descriptions
         if (path.extname(file) == ".desc") {
-            var filepath = path.join(snippets_dir, file)
-            var text = fs.readFileSync(filepath,'utf8');
-            library_desc[file]=text;
+            library_desc[file] = text;
+        } else {
+            lines = text.split("\n");
+            desc = ""; rest = "";
+            lines.forEach(line => {
+                if (line.startsWith("#")) {
+                    desc += line;
+                } else {
+                    rest += line + "\n";
+                }                
+            });
+            snippets[desc] = rest.trim();
         }
     });
 });
@@ -26,38 +42,37 @@ const myRepl = repl.start({prompt: "NQL> ", ignoreUndefined: true});
 
 Object.assign(myRepl.context,{
     help() {
-        console.log("howto(keywords)            searches for snippets from string of keywords");
-        console.log("list_packages(keywords)    searches for packages from string of keywords");        
-        console.log("howto_tasks                searches for snippets from tasks");
-        console.log("tasks                      lists all catalogued tasks");
+        console.log("list_s(keywords)           searches for snippets from string of keywords");
+        console.log("list_p(keywords)           searches for packages from string of keywords");        
         console.log("version                    prints version of library");
     }});
 
+/* list_snippets */
 Object.assign(myRepl.context,{
-    howto(string) {
-        let keywords = string.split(" ");
-        keywords.forEach(key => { console.log(key) })
+    list_s(keys_string) {
+        const keys = Object.keys(snippets);
+        keys.forEach(key => {
+            /* this is a nesty hack. should take code into account*/
+            doc = key;
+            body = snippets[key];
+            var sim = string_similarity.compareTwoStrings(keys_string, doc /* description of snippet */);
+            if (sim >= threshold_sim) {
+                console.log(doc);
+                console.log(body);
+            }
+        });
+        
     }});
 
+/* list_packages */
 Object.assign(myRepl.context,{
-    list_packages(string) {
-        let keywords = string.split(" ");
-        //TODO: search for keywords
-        console.log(library_desc);
+    list_p(string) {
+        for ([key, val] of Object.entries(library_desc)) {
+            console.log(`${key}      ${val}`);
+        }
     }});
 
-Object.assign(myRepl.context,{
-    howto_tasks() {
-        console.log(`--- ONLY listing library descriptions ---}`);
-        console.log(library_desc);
-    }});
-
-Object.assign(myRepl.context,{
-    tasks() {
-        console.log(`--- Not yet implemented ---}`);        
-    }});
-
-
+/* version */
 Object.assign(myRepl.context,{
     version() {
         console.log(`Node Query Library (NQL) version ${version}`);        
