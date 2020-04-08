6b2f16cd76fd81bc6024089c13ac6f010207aa06
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2020-04-08 15:01:38 -0300

    parsing

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const fs = require('fs');
 const path = require('path');
 const string_similarity = require('string-similarity');
 const natural = require('natural');
-
+const en = require("stopwords").english;
+const acorn = require("acorn");
+const acornwalk = require("acorn-walk");
 
 /* constants */
 const version = "1.0.0";
 const snippets_dir = "./snippets";
 const threshold_sim = 0.25;
 const tname = "NQL";
+const NUM_KEYWORDS = 5;
 
 /* library description */
 const library_desc = {};
@@ -29,7 +32,7 @@ fs.readdir(snippets_dir, (err, files) => {
         // update dictionaries with library and snippet descriptions
         if (path.extname(file) == ".desc") {
             library_desc[file] = text;
-            tfidf.addDocument(text);
+            tfidf.addDocument(removeStopWords(text));
         } else {
             lines = text.split("\n");
             desc = ""; rest = "";
@@ -40,17 +43,35 @@ fs.readdir(snippets_dir, (err, files) => {
                     rest += line + "\n";
                 }                
             });
-            tfidf.addDocument(desc);
-            tfidf.addDocument(rest);
+            tfidf.addDocument(removeStopWords(desc));
+            tfidf.addDocument(removeStopWords(parseJS(rest)));
             snippets[desc] = rest.trim();
         }
     });
 });
 
+/* remove stopwords from text */
+function removeStopWords(text) {
+    textClean = "" 
+    text.split(" ").forEach(s => { if (!en.includes(s.trim())) textClean = textClean+" "+s })
+    return textClean;
+}
+
+function parseJS(text) {
+    set = new Set();
+    acornwalk.simple(acorn.parse(text), {
+        Identifier(node) {
+            set.add(node.name);
+        }
+    })
+    res = ""
+    set.forEach(s => {res = res + " " + s})
+    return res
+}
+
 /* auto-completion function passed to repl.start as option. See:
  * https://nodejs.org/api/readline.html#readline_use_of_the_completer_function */
 function completer(line) {
-
     /* showing a list of keywords with lowest (more popular) tfidfs */
     const terms = {}
     for (a=0; a<tfidf.documents.length; a++) {
@@ -60,11 +81,12 @@ function completer(line) {
     }
     invTerms={}; Object.keys(terms).forEach(key => invTerms[terms[key]] = key)
     const sortedKeys = Object.keys(invTerms).map(parseFloat).sort(function(a,b){return a-b;})
-    const common_tfidfs = sortedKeys.slice(0,4); //TODO: create a constant
+    const common_tfidfs = sortedKeys.slice(0, NUM_KEYWORDS); //TODO: create a constant
     completions = []
     common_tfidfs.forEach(val => { completions.push(invTerms[val]) })
     
     // completions
+
     const hits = completions.filter((c) => c.startsWith(line));
     // Show all completions if none found
     return [hits.length ? hits : completions, line];
