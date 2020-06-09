e123b89325d81788e1e34fda96ff4da2310c496d
Brittany Reid <39616756+Brittany-Reid@users.noreply.github.com>
2020-06-09 22:49:57 +0930

    Random Snippet Analysis

--- analysis/readmes.js
+++ analysis/readmes.js
@@ -2,8 +2,9 @@ const fs = require("fs");
 const path = require("path");
 const htmlparser = require("htmlparser2");
 
-/**
- * Analyse the contents of the 100 most depended upon packages.
+
+/*
+ * Analyse snippets extracted from a random sample of packages.
  */
 
 var dir = __dirname;
@@ -15,28 +16,195 @@ if (
   dir = path.join(dir, "../");
 }
 const BASE = dir;
-const README_DIR = path.join(BASE, "data/100MostDependedUpon.json");
+const DATABASE_DIR = path.join(BASE, "data/readmes.json");
+const README_DIR = path.join(BASE, "data/SampleReadmes.json");
+const SNIPPET_DIR = path.join(BASE, "analysis/results/snippets.txt")
+const StreamObject = require("stream-json/streamers/StreamObject");
 const FENCE = /^\`\`\`([\w_-]+)?\s*$/;
 const HEADER = /^#+\s+\S+/;
 
-var snippetSet = {};
+/**
+ * Gets 384 random readme files and saves them into README_DIR.
+ */
 
-var allSnippets = [];
+ async function getRandomReadmes(){
 
-function removeHTML(readme) {
-  var result = [];
-  var parser = new htmlparser.Parser(
-    {
-      ontext: function (text) {
-        result.push(text);
-      },
-    },
-    { decodeEntities: true }
-  );
+  //first, get number of packages
+  var counter = 0;
 
-  parser.write(readme);
 
-  return result;
+  function onData(){
+    counter++;
+  }
+
+  function onEnd(){
+    console.log(counter);
+  }
+
+  //await readFile(onData, onEnd);
+
+  counter = 925317; //if we know the value already
+
+  //get 384 random indexes
+  var samples = 384;
+  var list = [];
+
+  //generate a list of 1-count
+  for(var i=0; i<counter; i++){
+    list.push(i);
+  }
+
+  list = shuffle(list);
+
+  //get the random sample
+  var sample = list.slice(0, samples);
+
+  //sort so we can look in order
+  sample.sort((a, b) => a - b);
+
+
+  //add to object
+  var readmes = {};
+  var counter2 = 0;
+  var i = 0;
+
+  function onData(data, pipeline){
+    var name = data["key"];
+    var readme = data["value"];
+    //add if matches in sample
+    if(counter2 == sample[i]){
+      //the package names are formatted for windows file names, change back
+      name = name.replace(/%2A/g, "*");
+      name = name.replace(/%2A/g, "/");
+      name = name.replace(/%2E/g, ".");
+      readmes[name] = readme;
+      //increment the next to look for
+      i++;
+    }
+    counter2++;
+  }
+
+  await readFile(onData);
+
+  //write readmes to file
+  fs.writeFileSync(README_DIR, JSON.stringify(readmes), {encoding:"utf-8"});
+
+ }
+
+/**
+ * Uses stream-json to load the database without storing it into memory.
+ * Returns a promise when the end event triggers.
+ */
+ async function readFile(onData = function(){}, onEnd = function(){}){
+  return promise = new Promise((resolve, reject) => {
+    //create pipeline
+    const pipeline = fs.createReadStream("data/readmes.json").pipe(StreamObject.withParser());
+
+    //on data, load into memory
+    pipeline.on("data", async (data) => {
+      onData(data, pipeline);
+    });
+    //when done, process
+    pipeline.on("end", (data) => {
+      onEnd();
+      resolve();
+    });
+  });
+ }
+
+ /**
+  * Array shuffle function using Fisher-Yates
+  * Copied from: https://stackoverflow.com/a/2450976
+  */
+ function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+/**
+ * Process for snippets.
+ */
+function main(){
+  var data = JSON.parse(fs.readFileSync(README_DIR));
+  var packages = Object.keys(data);
+
+  var allSnippets = [];
+  
+  for (let i = 0; i < packages.length; i++) {
+    var name = packages[i];
+    var readme = data[name];
+    
+    //get snippets
+    var snippets = getSnippets(readme, name);
+    
+    // console.log(snippets);
+    
+    
+    allSnippets.push(snippets);
+  }
+
+  //console.log(JSON.stringify(allSnippets, null, 1))
+
+  fs.writeFileSync(SNIPPET_DIR, JSON.stringify(allSnippets, null, 1), {encoding: "utf-8"});
+}
+
+/**
+ * Extract snippets from readme file.
+ * Ignore code blocks marked with non-js
+ */
+function getSnippets(readme, package) {
+  var snippets = [];
+  var snippetObject = {};
+  var snippetList = {};
+  snippetList["package"] = package;
+
+
+  var lines = readme.split("\n");
+  var block = false;
+  var opening;
+  var start = -1;
+  lines.forEach((line, index) => {
+    var match = line.match(FENCE);
+    if (match) {
+      if (block == false) {
+        //start new code block
+        block = "";
+        opening = match;
+        start = index;
+      } else {
+        //finish block
+        //ignore anything explicitly marked as non js
+        if (opening[0] == "```js" || opening[0] == "```") {
+          snippetObject["description"] = getDescription(lines, start);
+          snippetObject["snippet"] = block;
+          snippets.push(snippetObject);
+          snippetObject = {};
+        }
+        block = false;
+      }
+    } else if (block !== false) {
+      block += line + "\n";
+    }
+  });
+
+  snippetList["snippets"] = snippets;
+
+  return snippetList;
 }
 
 /**
@@ -62,67 +230,6 @@ function getDescription(lines, start){
   return description;
 }
 
-/**
- * Extract snippets from readme file.
- * Ignore code blocks marked with non-js
- */
-function getSnippets(readme, package) {
-  var snippets = [];
-  var snippetObject = {};
-
-  var lines = readme.split("\n");
-  var block = false;
-  var opening;
-  var start = -1;
-  lines.forEach((line, index) => {
-    var match = line.match(FENCE);
-    if (match) {
-      if (block == false) {
-        //start new code block
-        block = "";
-        opening = match;
-        start = index;
-      } else {
-        //finish block
-        //ignore anything explicitly marked as non js
-        if (opening[0] == "```js" || opening[0] == "```") {
-          snippetObject["package"] = package;
-          snippetObject["description"] = getDescription(lines, start);
-          snippetObject["snippet "] = block;
-          snippets.push(snippetObject);
-          snippetObject = {};
-        }
-        block = false;
-      }
-    } else if (block !== false) {
-      block += line + "\n";
-    }
-  });
-
-  return snippets;
-}
-
-async function main() {
-  var data = JSON.parse(fs.readFileSync(README_DIR));
-  var packages = Object.keys(data);
-
-  for (let i = 0; i < packages.length; i++) {
-    var name = packages[i];
-    var readme = data[name];
-
-    //get snippets
-    var snippets = getSnippets(readme, name);
-
-    console.log(snippets);
-
-
-    //remove html elements
-    //readme = removeHTML(readme);
-
-    allSnippets = allSnippets.concat(snippets);
-  }
-
-  
-}
-
+//comment out unless u need to generate a new set of readmes
+//getRandomReadmes();
 main();
---
+++ tests/base-prompt-tests.js
@@ -0,0 +1,203 @@
+require("mocha");
+var assert = require("assert");
+const BasePrompt = require("../ncq/ui/prompts/base-prompt");
+const sinon = require("sinon");
+const PromptHandler = require("../ncq/ui/prompt-handler");
+const { getDefault } = require("../ncq/config");
+
+const tab = {
+  name: "tab",
+  sequence: "\t",
+  raw: "\t",
+  ctrl: false,
+  meta: false,
+  shift: false,
+  option: false,
+};
+
+/**
+ * Utility function, takes a string to send as input to Enquirer prompt
+ * and simulates keypresses.
+ */
+async function send(string, prompt) {
+  //send each character as a keypress
+  for (let i = 0; i < string.length; i++) {
+    await prompt.keypress(string[i]);
+  }
+  //finish
+  await prompt.submit();
+}
+
+/**
+ * Unit tests for the BasePrompt class
+ */
+describe("BasePrompt", function () {
+  var output = [];
+
+  /**
+   * This runs once, before tests.
+   * We hide output and store it in an array so we can check what gets printed.
+   */
+  before(() => {
+    // stub console log to push to an array
+    sinon.stub(console, "log").callsFake(function (string) {
+      output.push(string);
+    });
+  });
+
+  /**
+   * This runs before each tests.
+   * The clear the output array.
+   */
+  this.beforeEach(() => {
+    output = [];
+  });
+
+  describe("unit tests", function () {
+    it("should allow custom non choice", async function () {
+      var prompt = new BasePrompt({ choices: ["a", "b"], show: false });
+
+      prompt.once("run", async () => {
+        prompt.keypress("c");
+        prompt.submit();
+      });
+
+      var out = await prompt.run();
+
+      assert.strictEqual(out, "c");
+    });
+    it("should allow empty", async function () {
+      var prompt = new BasePrompt({ choices: ["a", "b"], show: false });
+
+      prompt.once("run", async () => {
+        prompt.submit();
+      });
+
+      var out = await prompt.run();
+
+      assert.strictEqual(out, "");
+    });
+    it("should toggle autocomplete and insert", async function () {
+      var prompt = new BasePrompt({ choices: ["a", "b"], show: false });
+
+      prompt.once("run", async () => {
+        //default toggle
+        prompt.keys["autocomplete"] = getDefault().keybindings.autocomplete;
+        await prompt.keypress("\t", tab);
+        await prompt.submit();
+        await prompt.submit();
+      });
+
+      var out = await prompt.run();
+
+      assert.strictEqual(out, "a");
+    });
+    it("should toggle autocomplete on and off", async function () {
+      var prompt = new BasePrompt({ choices: ["a", "b"], show: false });
+
+      prompt.once("run", async () => {
+        //default toggle
+        prompt.keys["autocomplete"] = getDefault().keybindings.autocomplete;
+        await prompt.keypress("\t", tab);
+        await prompt.keypress("\t", tab);
+        await prompt.submit();
+      });
+
+      var out = await prompt.run();
+
+      assert.strictEqual(out, "");
+    });
+    it("should allow custom when toggled", async function () {
+      var prompt = new BasePrompt({ choices: ["a", "b"], show: false });
+
+      prompt.once("run", async () => {
+        //default toggle
+        prompt.keys["autocomplete"] = getDefault().keybindings.autocomplete;
+        await prompt.keypress("\t", tab);
+        await send("abcd", prompt);
+        await prompt.submit();
+      });
+
+      var out = await prompt.run();
+
+      assert.strictEqual(out, "abcd");
+    });
+    it("should fix undefined", async function () {
+      var prompt = new BasePrompt({ choices: ["a", "b"], show: false });
+
+      var ctrlright = {
+        name: "right",
+        ctrl: true,
+        meta: false,
+        option: false,
+        shift: false,
+      };
+
+      prompt.once("run", async () => {
+        //overwrite key bindings
+        prompt.keys = {};
+        prompt.keypress(undefined, ctrlright);
+        prompt.submit();
+      });
+
+      var out = await prompt.run();
+
+      assert.notEqual(out, "undefined");
+    });
+    it("should not print box from ctrlc", async function () {
+      //sometimes ctrl c prints a box
+      var prompt = new BasePrompt({ choices: ["a", "b"], show: false });
+
+      var escape = {
+        raw: "\u0003",
+      };
+
+      prompt.once("run", async () => {
+        prompt.keypress(undefined, escape);
+        prompt.submit();
+      });
+
+      var out = await prompt.run();
+
+      assert.notEqual(out, "\u0003");
+    });
+    it("should allow empty choices", async function () {
+      //sometimes ctrl c prints a box
+      var prompt = new BasePrompt({ choices: [], show: false });
+
+      prompt.once("run", async () => {
+        prompt.submit();
+      });
+
+      var out = await prompt.run();
+
+      assert.strictEqual(out, "");
+    });
+    it("should allow multiple inserts", async function () {
+      //sometimes ctrl c prints a box
+      var prompt = new BasePrompt({ choices: ["a", "b"], show: false });
+
+      prompt.once("run", async () => {
+        prompt.keys["autocomplete"] = getDefault().keybindings.autocomplete;
+        await prompt.keypress("\t", tab);
+        prompt.submit();
+        await prompt.keypress("\t", tab);
+        prompt.submit();
+        await prompt.keypress("\t", tab);
+        prompt.submit();
+        prompt.submit();
+      });
+
+      var out = await prompt.run();
+
+      assert.strictEqual(out, "aaa");
+    });
+  });
+
+  /**
+   * After all tests, restore functions.
+   */
+  after(() => {
+    console.log.restore();
+  });
+});
