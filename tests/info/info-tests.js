b411e6a7e0cf335094598a156e8d9cc4661ecd9b
Brittany Reid <39616756+Brittany-Reid@users.noreply.github.com>
2020-11-11 15:33:30 +1030

    add stats test cases for the paper
    can be ran using 'mocha info'. I kept a branch for the version we evaluated and tried the release feature, but it just saves the source code so that's not too useful. i wont be adding anything to the tool besides these tests to confirm the numbers in the paper.

---
+++ tests/info/info-tests.js
@@ -0,0 +1,46 @@
+require("mocha");
+const { doesNotMatch } = require("assert");
+var assert = require("assert");
+const path = require("path");
+const DataHandler = require("../../ncq/core/data-handler");
+const { getConfig } = require("../../ncq/config");
+const { getBaseDirectory } = require("../../ncq/utils");
+
+var config = getConfig();
+const BASE_DIR = getBaseDirectory();
+const SNIPPET_DIR = path.join(BASE_DIR, config.get("files.snippets"));
+const SNIPPET_DB = path.join(BASE_DIR, config.get("files.snippetDB"));
+const INFO_DIR = path.join(BASE_DIR, config.get("files.info"));
+const INFO_DB = path.join(BASE_DIR, config.get("files.packageDB"));
+const TASK_DIR = path.join(BASE_DIR, config.get("files.tasks"));
+
+/**
+ * Stats for the paper :)
+ */
+
+describe("Info", function () {
+    this.timeout(0);
+
+    /**
+     * Runs once. Makes a global datahandler.
+     */
+    before(() => {
+        data = new DataHandler();
+        data.loadPackages(INFO_DIR, INFO_DB);
+        data.loadSnippets(SNIPPET_DIR, SNIPPET_DB);
+        data.loadTasks(TASK_DIR);
+    });
+
+
+    it("How many packages?", function () {
+        console.log("Packages: " + data.nameToId.size);
+    });
+
+    it("How many snippets?", function(){
+        console.log("Snippets: " + data.snippets.length);
+    });
+
+    it("How many tasks?", function(){
+        console.log("Tasks: " + data.tasks.size);
+    });
+});
